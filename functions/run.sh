77d752660c94038ee86a68ddacc11f2090fcf07a
Felix Halim <felix.halim@gmail.com>
2019-03-27 09:47:21 -0700

    Add run.sh

---
+++ functions/run.sh
@@ -0,0 +1,19 @@
+case "$1" in
+
+ssh)
+    gcloud compute --project "kawal-c1" ssh --zone "us-central1-c" "admin"
+    ;;
+
+admin)
+    (cd ../shared; npm run build) && \
+    npm run build && \
+    node --max-old-space-size=5120 lib/admin.js
+    ;;
+
+referral)
+    (cd ../shared; npm run build) && \
+    npm run build && \
+    node --max-old-space-size=1024 lib/referral.js
+    ;;
+
+esac
--- functions/ssh.sh
+++
@@ -1,1 +0,0 @@
-gcloud compute --project "kawal-c1" ssh --zone "us-central1-c" "admin"
