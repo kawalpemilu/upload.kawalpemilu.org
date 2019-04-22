3a9c429b6c3a992d3f9e4c2b1ae740b8a3b644d0
Felix Halim <felix.halim@gmail.com>
2019-04-21 18:07:23 -0700

    Combine fetch and upload KPU scan script.

--- functions/src/tester.ts
+++ functions/src/tester.ts
@@ -3,6 +3,7 @@ import * as request from 'request-promise';
 import * as fs from 'fs';
 import * as util from 'util';
 import * as crypto from 'crypto';
+import { spawn } from 'child_process';
 
 import {
   UploadRequest,
@@ -35,12 +36,121 @@ const bucket = admin.storage().bucket('kawal-c1.appspot.com');
 const readFileAsync = util.promisify(fs.readFile);
 const statAsync = util.promisify(fs.stat);
 
+const KPU_API = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp';
+const KPU_WIL = 'https://pemilu2019.kpu.go.id/static/json/wilayah';
+
+const LOCAL_FS = '/Users/felixhalim/Projects/kawal-c1/kpu/cache';
+const proxy = 'socks5h://localhost:12345';
+const isOffline = true;
+
 interface User {
   uid: string;
   idToken: string;
   refreshToken: string;
   expiresIn: number;
   expiresAt: number;
+}
+
+async function curl(url): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const c = spawn('curl', ['-s', '--proxy', proxy, url]);
+    let s = '';
+    c.stdout.on('data', data => (s += data));
+    c.on('close', code => {
+      if (code) reject(new Error(`Exit code ${code}`));
+      else resolve(s);
+    });
+  });
+}
+
+async function download(url, output): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const params = ['-s', '--proxy', proxy, '--output', output, url];
+    const c = spawn('curl', params);
+    c.on('close', code => {
+      if (code) reject(new Error(`Exit code ${code}`));
+      else resolve();
+    });
+  });
+}
+
+async function downloadWithRetry(url, output) {
+  try {
+    await download(url, output);
+    const s = fs.statSync(output);
+    if (s.size < 10 << 10) {
+      // console.error('image too small', url);
+      fs.unlinkSync(output);
+    } else {
+      console.log('downloaded', url);
+    }
+    return;
+  } catch (e) {
+    console.error('download retry', e.message);
+  }
+}
+
+async function getWithRetry(url) {
+  console.log('fetching', url);
+  for (let i = 0; i < 3; i++) {
+    try {
+      return await curl(url);
+    } catch (e) {
+      console.error('get retry', i, e.message);
+    }
+  }
+  return JSON.stringify({ table: {}, images: [] });
+}
+
+async function getCached(url, cachedFilename, inProgressFn) {
+  try {
+    const cache = JSON.parse(fs.readFileSync(cachedFilename, 'utf8'));
+    if (isOffline) return cache;
+    if (!inProgressFn(cache)) return cache;
+  } catch (e) {}
+
+  try {
+    const res = JSON.parse(await getWithRetry(url));
+    fs.writeFileSync(cachedFilename, JSON.stringify(res));
+    return res;
+  } catch (e) {
+    console.error(e.message);
+    return { table: {}, images: [] };
+  }
+}
+
+function getPathUrlPrefix(prefix, path) {
+  let url = prefix;
+  for (let i = 1; i < path.length; i++) {
+    url += `/${path[i]}`;
+  }
+  return url;
+}
+
+async function fetchImageJson(imageId, path) {
+  const dir = LOCAL_FS + '/' + path[path.length - 1];
+  try {
+    fs.mkdirSync(dir);
+  } catch (e) {}
+
+  return getCached(
+    getPathUrlPrefix(KPU_API, path) + `/${imageId}.json`,
+    dir + `/${imageId}.json`,
+    c => c.images.length < 2
+  );
+}
+
+async function downloadImage(fn, imageId, path) {
+  if (isOffline) return;
+  const a = imageId.substring(0, 3);
+  const b = imageId.substring(3, 6);
+  const url = `https://pemilu2019.kpu.go.id/img/c/${a}/${b}/${imageId}/${fn}`;
+
+  const dir = LOCAL_FS + '/' + path[path.length - 1];
+  const filename = dir + `/${fn}`;
+  if (!fs.existsSync(filename)) {
+    await downloadWithRetry(url, filename);
+  }
 }
 
 function md5(str) {
@@ -433,13 +543,31 @@ async function kpuUploadImage(kelId, tpsNo, filename: string) {
   if (!res.ok) throw new Error(JSON.stringify(res, null, 2));
 }
 
-async function kpuUploadDir(kelId: number) {
-  const LOCAL_FS = '/Users/felixhalim/Projects/kawal-c1/kpu/cache';
-  const hashdir = LOCAL_FS + '/../uploaded-md5';
-  const dir = LOCAL_FS + `/${kelId}`;
-  if (!fs.existsSync(dir)) return;
+async function fetchKelImages(kelId: number, path) {
+  const inProgressFn = c => c.progress.proses < c.progress.total;
+  const url = getPathUrlPrefix(KPU_API, path) + '.json';
+  const res = await getCached(url, `${LOCAL_FS}/${kelId}.json`, inProgressFn);
+  const arr = Object.keys(res.table).filter(id => res.table[id]['21'] !== null);
+  const imgJson = {};
+  await Promise.all(
+    arr.map(async imageId => {
+      const i = await fetchImageJson(imageId, path);
+      if (!i.images) {
+        console.error('null images', kelId, path, imageId);
+        return;
+      }
+      imgJson[imageId] = i;
+      for (const fn of i.images) {
+        await downloadImage(fn, imageId, path);
+      }
+    })
+  );
+  return imgJson;
+}
 
+function getNewFileHashes(dir) {
   const newFileHashes = {};
+  const hashdir = LOCAL_FS + '/../uploaded-md5';
   fs.readdirSync(dir).forEach(file => {
     if (!file.endsWith('.jpg')) return;
     const filename = dir + `/${file}`;
@@ -451,60 +579,78 @@ async function kpuUploadDir(kelId: number) {
 
     newFileHashes[file] = hashFile;
   });
-
-  if (!Object.keys(newFileHashes).length) return;
-
-  // console.log('newFIleHashes', newFileHashes);
-
-  const res = JSON.parse(fs.readFileSync(`${LOCAL_FS}/${kelId}.json`, 'utf8'));
-  const arr = Object.keys(res.table).filter(id => res.table[id]['21'] !== null);
-  const wil = JSON.parse(fs.readFileSync(`${LOCAL_FS}/w${kelId}.json`, 'utf8'));
-  for (const key of arr) {
-    const s = wil[key].nama.split(' ');
-    let tpsNo = parseInt(s[1], 10);
-    if (s[0] === 'TPS') {
-      // Ignored.
-    } else if (s[0] === 'POS') {
-      tpsNo += 1000;
-    } else if (s[0] === 'KSK') {
-      tpsNo += 2000;
-    }
-
-    const img = JSON.parse(fs.readFileSync(`${dir}/${key}.json`, 'utf8'));
-    for (const fn of img.images) {
-      if (newFileHashes[fn]) {
-        try {
-          await kpuUploadImage(kelId, tpsNo, `${dir}/${fn}`).then(() =>
-            fs.writeFileSync(newFileHashes[fn], '1', 'utf8')
-          );
-        } catch (e) {
-          console.error('failed upload', kelId, e.message);
-        }
-        delete newFileHashes[fn];
-      }
-    }
-  }
-
-  if (Object.keys(newFileHashes).length) throw new Error();
+  return newFileHashes;
 }
 
-async function kpuUpload() {
-  const paralellism = 50;
+async function kpuUploadKel(kelId: number, path) {
+  const imgJson = await fetchKelImages(kelId, path);
+  const dir = LOCAL_FS + `/${kelId}`;
+  if (!fs.existsSync(dir)) return;
+
+  const newFileHashes = getNewFileHashes(dir);
+  if (!Object.keys(newFileHashes).length) return;
+  // console.log('newFIleHashes', newFileHashes);
+
+  const wurl = getPathUrlPrefix(KPU_WIL, path) + '.json';
+  const wil = await getCached(wurl, `${LOCAL_FS}/w${kelId}.json`, c => false);
   const promises = [];
-  for (let i = 0; i < paralellism; i++) {
-    promises.push(Promise.resolve());
-  }
-  let idx = 0;
-  for (const id of Object.keys(H)) {
-    const h = H[id];
-    if (h.depth === 4) {
-      promises[idx] = promises[idx].then(() => kpuUploadDir(+id));
-      idx = (idx + 1) % paralellism;
+  for (const imageId of Object.keys(imgJson)) {
+    const s = wil[imageId].nama.split(' ');
+    const add =
+      s[0] === 'TPS' ? 0 : s[0] === 'POS' ? 1000 : s[0] === 'KSK' ? 2000 : -1;
+    if (add < 0) throw new Error();
+    const tpsNo = parseInt(s[1], 10) + add;
+    for (const fn of imgJson[imageId].images) {
+      if (!newFileHashes[fn]) continue;
+      promises.push(
+        kpuUploadImage(kelId, tpsNo, `${dir}/${fn}`)
+          .then(() => {
+            fs.writeFileSync(newFileHashes[fn], '1', 'utf8');
+            delete newFileHashes[fn];
+          })
+          .catch(e => console.error('upload failed', kelId, tpsNo, e.message))
+      );
     }
   }
-  console.log('upload setup');
   await Promise.all(promises);
-  console.log('upload done');
+
+  const leftover = Object.keys(newFileHashes).length;
+  if (leftover) console.error('Upload leftover', leftover);
+}
+
+async function kpuUpload(id, depth, opath) {
+  const path = opath.slice();
+  path.push(id);
+  const url = getPathUrlPrefix(KPU_API, path) + '.json';
+  const inProgressFn = c => c.progress.proses < c.progress.total;
+  const res = await getCached(url, `${LOCAL_FS}/${id}.json`, inProgressFn);
+  const arr = Object.keys(res.table).filter(
+    key => res.table[key]['21'] !== null
+  );
+
+  if (depth === 4) {
+    await kpuUploadKel(id, path);
+    return;
+  }
+
+  if (depth <= 2) {
+    for (const cid of arr) {
+      if (id === -99) {
+        const cpath = path.slice();
+        for (let i = 2; i > 0; i--) {
+          cpath.push(+cid + i);
+        }
+        await kpuUpload(+cid, depth + 3, cpath);
+      } else {
+        await kpuUpload(+cid, depth + 1, path);
+      }
+    }
+    return;
+  }
+
+  await Promise.all(arr.map(cid => kpuUpload(+cid, depth + 1, path)));
+  const h = H[id] as HierarchyNode;
+  console.log('done', id, h.name, h.parentNames);
 }
 
 async function fixPemandangan() {
@@ -531,6 +677,152 @@ async function whoChangedRole() {
   }
 }
 
+async function fixHierarchy() {
+  async function recHie(id, depth, opath) {
+    const path = opath.slice();
+    path.push(id);
+
+    const h = H[id];
+    if (!h) throw new Error(`unknown id ${id} ${path}`);
+
+    const url =
+      getPathUrlPrefix(KPU_WIL, depth === 0 ? path.concat(0) : path) + '.json';
+    const res = await getCached(url, `${LOCAL_FS}/w${id}.json`, c => false);
+
+    let nChildren = 0;
+    if (depth === 4) {
+      const exTps = {};
+      h.children.forEach(c => {
+        exTps[c[0]] = 1;
+        if (c[0] > 1000 && id >= 0) {
+          throw new Error('toobig ' + id + ' ' + c[0]);
+        }
+      });
+
+      for (const tid of Object.keys(res)) {
+        const s = res[tid].nama.split(' ');
+        if (s[0] !== 'TPS' && s[0] !== 'POS' && s[0] !== 'KSK')
+          throw new Error(`nama: ${s[0]}`);
+        const tpsNo = parseInt(s[1], 10);
+        if (!exTps[tpsNo] && id >= 0) {
+          let j = h.children.length;
+          for (let i = 0; i < h.children.length; i++) {
+            if (h.children[i][0] > tpsNo) {
+              j = i;
+              break;
+            }
+          }
+          console.error('Missing TPS', id, tpsNo, h.children);
+          h.children.splice(j, 0, [tpsNo, -1, -1]);
+        }
+        nChildren++;
+      }
+    } else {
+      const cids = {};
+      h.children.forEach(c => (cids[c[0]] = 1));
+
+      if (depth < 2) {
+        for (const cid of Object.keys(res)) {
+          if (!cids[cid]) throw new Error();
+          nChildren++;
+          await recHie(+cid, depth + 1, path);
+        }
+      } else {
+        const promises = [];
+        for (const key of Object.keys(res)) {
+          const cid = +key;
+          const cname = res[cid].nama;
+          if (!cids[cid]) {
+            if (H[cid]) {
+              console.log('ADA TAPI MISSING', cid, cname, id);
+              if (H[cid].depth !== 4) throw new Error(H[cid].depth);
+              nChildren--;
+            } else {
+              const names = h.parentNames.slice();
+              names.push(h.name);
+              const nTps = await addTpsLn(cid, cname, depth + 1, path, names);
+              h.children.push([cid, cname, nTps, -1, -1]);
+              console.log('\n\n\nadded', cid, cname);
+            }
+          }
+          nChildren++;
+          promises.push(recHie(+cid, depth + 1, path));
+        }
+        await Promise.all(promises);
+      }
+      if (
+        nChildren > h.children.length &&
+        id !== 83553 &&
+        id !== 72398 &&
+        id !== 930135
+      ) {
+        // console.error('here', nChildren, h.children.length, path, res, h);
+        throw new Error(`Child mismatch ${nChildren} != ${h.children.length}`);
+      }
+    }
+  }
+
+  async function addTpsLn(id, name, depth, parentIds, parentNames) {
+    if (H[id]) throw new Error();
+    const children = [];
+    H[id] = { id, name, depth, parentIds, parentNames, children, data: {} };
+
+    const path = parentIds.slice();
+    path.push(id);
+    const names = parentNames.slice();
+    names.push(name);
+    const url = getPathUrlPrefix(KPU_WIL, path) + '.json';
+    const res = await getCached(url, `${LOCAL_FS}/w${id}.json`, c => false);
+    // console.log(id, res);
+
+    if (depth === 4) {
+      for (const key of Object.keys(res)) {
+        const s = res[key].nama.split(' ');
+        let tpsNo = parseInt(s[1], 10);
+        if (tpsNo > 795) throw new Error('' + tpsNo);
+        if (s[0] === 'TPS') {
+          // if (tpsNo > 90) throw new Error('' + tpsNo);
+        } else if (s[0] === 'POS') {
+          tpsNo += 1000;
+        } else if (s[0] === 'KSK') {
+          tpsNo += 2000;
+        }
+        children.push([tpsNo, -1, -1]);
+      }
+      console.log('t', H[id]);
+      return children.length;
+    }
+
+    let totTps = 0;
+    for (const key of Object.keys(res)) {
+      const cid = +key;
+      const cname = res[cid].nama;
+      const nTps = await addTpsLn(cid, cname, depth + 1, path, names);
+      children.push([cid, cname, nTps, -1, -1]);
+      totTps += nTps;
+    }
+    console.log('h', H[id]);
+    return totTps;
+  }
+
+  if (!H[-99]) {
+    const nTps = await addTpsLn(-99, 'Luar Negeri', 1, [0], ['IDN']);
+    H[0].children.push([-99, 'Luar Negeri', nTps, -1, -1]);
+    console.log('Tot TPS LN', nTps);
+  }
+
+  const m = H[18880].children.find(a => a[0] === 931304);
+  if (m) {
+    m[0] = 18887;
+    console.log(H[18880]);
+  }
+
+  await recHie(0, 0, []);
+
+  const hieFn = '/Users/felixhalim/Projects/kawal-c1/kpu/hierarchy.js';
+  fs.writeFileSync(hieFn, `exports.H = ${JSON.stringify(H)};`);
+}
+
 // parallelUpload().catch(console.error);
 // loadTest().catch(console.error);
 // fixClaimersRole().catch(console.error);
@@ -538,5 +830,6 @@ async function whoChangedRole() {
 // checkHierarchy().catch(console.error);
 // fixPemandangan().catch(console.error);
 // whoChangedRole().catch(console.error);
+// fixHierarchy().catch(console.error);
 
-kpuUpload().catch(console.error);
+kpuUpload(0, 0, []).catch(console.error);
